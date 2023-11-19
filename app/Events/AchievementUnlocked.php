4c4957cdf8c9fadf49d29a4d72d5eaeb7695da35
mazinoukah <ewomaukah@yahoo.com>
2023-11-19 13:10:15 +0100

    Create achievement unlocked event

---
+++ app/Events/AchievementUnlocked.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AchievementUnlocked
+{
+    use Dispatchable, SerializesModels;
+
+    public string $achievementName;
+    public User $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(string $achievementName, $user)
+    {
+        $this->achievementName = $achievementName;
+        $this->user = $user;
+    }
+}
