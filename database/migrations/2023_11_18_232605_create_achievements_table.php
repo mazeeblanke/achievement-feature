8b1b1d190bb87030c3dbf35db475593fdb11025b
mazinoukah <ewomaukah@yahoo.com>
2023-11-19 01:30:22 +0100

    Add unique keys to achievements

--- database/migrations/2023_11_18_232605_create_achievements_table.php
+++ database/migrations/2023_11_18_232605_create_achievements_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->integer('qualifier')->nullable(false);
             $table->foreignId('achievement_type_id')->nullable(false)->constrained();
+            $table->unique(['name', 'qualifier']);
             $table->timestamps();
         });
     }
