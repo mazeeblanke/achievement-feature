40cced30a94d19f42177e11230863a59cf07eb45
mazinoukah <ewomaukah@yahoo.com>
2023-11-20 09:29:54 +0100

    Add BadgeUnlocked Event

---
+++ app/Events/BadgeUnlocked.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BadgeUnlocked
+{
+    use Dispatchable, SerializesModels;
+
+    public string $badgeName;
+    public User $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(string $badgeName, User $user)
+    {
+        $this->badgeName = $badgeName;
+        $this->user = $user;
+    }
+}
