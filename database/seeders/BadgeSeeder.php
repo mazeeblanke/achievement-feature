e148b457a929b929797dd7bbd25046ac333a61e5
mazinoukah <ewomaukah@yahoo.com>
2023-11-20 09:32:24 +0100

    Clean up badge seeder

--- database/seeders/BadgeSeeder.php
+++ database/seeders/BadgeSeeder.php
@@ -13,29 +13,23 @@ class BadgeSeeder extends Seeder
     public const ADVANCED = 'Advanced';
     public const MASTER = 'Master';
 
+    public const BADGES = [
+        0 => self::BEGINNER,
+        4 => self::INTERMEDIATE,
+        8 => self::ADVANCED,
+        10 => self::MASTER,
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Badge::firstOrCreate([
-            'name' => self::BEGINNER,
-            'no_of_achievements' => 0,
-        ]);
-
-        Badge::firstOrCreate([
-            'name' => self::INTERMEDIATE,
-            'no_of_achievements' => 4,
-        ]);
-
-        Badge::firstOrCreate([
-            'name' => self::ADVANCED,
-            'no_of_achievements' => 8,
-        ]);
-
-        Badge::firstOrCreate([
-            'name' => self::MASTER,
-            'no_of_achievements' => 10,
-        ]);
+        foreach (self::BADGES as $noOfAchievements => $name) {
+            Badge::firstOrCreate([
+                'name' => $name,
+                'no_of_achievements' => $noOfAchievements,
+            ]);
+        }
     }
 }
